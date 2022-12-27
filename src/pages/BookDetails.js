d3114428f3248087115eff03b18835edc87aed08
genesis331 <cheah3838@gmail.com>
2022-12-27 17:55:28 +0800

    enable download

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useMemo, useState} from "react";
 import {Button, Divider, SplitButtonGroup, Typography} from "@douyinfe/semi-ui";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 const useQuery = () => {
     const { search } = useLocation();
@@ -9,7 +9,7 @@ const useQuery = () => {
 
 function BookDetails() {
     let query = useQuery();
-    
+    let navigate = useNavigate();
     const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://bookfinderapi.zixucheah331.ml";
     const { Paragraph, Title, Text } = Typography;
 
@@ -57,8 +57,9 @@ function BookDetails() {
                         </div>
                         <div>
                             <SplitButtonGroup>
-                                <Button theme="solid" type="primary" size="large">Download</Button>
+                                <Button theme="solid" type="primary" size="large" onClick={() => {if (bookData) {window.open(`http://library.lol/main/${bookData.md5}`)}}}>Download</Button>
                             </SplitButtonGroup>
+                            <Text type="tertiary" className="ml-3">{bookData ? `(${bookData.extension})` : ""}</Text>
                         </div>
                     </div>
                 </div>
