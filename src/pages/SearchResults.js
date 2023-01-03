b5d0bfff869d1f1a1a1c49321a0d1d9d06ee3b88
genesis331 <cheah3838@gmail.com>
2023-01-03 09:52:22 +0800

    revert changes

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -51,8 +51,7 @@ function SearchResults({ modifyHeaderValue }) {
         modifyHeaderValue(query.get("q"));
         async function fetchData() {
             if (query.get("q")) {
-                let results = await fetch(`${apiUrl}/search?q=${query.get("q")}`, {mode: "cors"});
-                console.log(results);
+                let results = await fetch(`${apiUrl}/search?q=${query.get("q")}`);
                 results = await results.json();
                 return results;
             }
